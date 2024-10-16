2f71658380b022e8b0d38a5c2844f4f37dad657e
Ivan Figueroa <Ivanfiggomez@gmail.com>
2024-10-16 10:38:40 -0400

    Added daily totals section with nutritional breakdown
    Added a section to display total daily nutritional values: calories, protein, fat, and carbs, Added the horizontal line for seperation of the next section

--- app/diary/page.jsx
+++ app/diary/page.jsx
@@ -45,23 +45,39 @@ function handleMealChange(event) {
     <div className="flex flex-col min-h-screen">
       <NavBar />
 
-    <div className="flex flex-row items-center justify-between">
-
+    <div className="flex flex-row items-center justify-between border border-black">
       <h1 className="text-5xl font-bold ml-200px">BiteLog</h1>
-
       <div className="flex justify-center w-full mr-250px">
-
-        <p>Your food log for:</p>
+        <p className = "text-2xl font-bold">Your food log for:</p>
         <button onClick={decreaseDate} className="mr-2">←</button>
         <input type="date" value={selectedDate} onChange={handleDateChange} />
         <button onClick={increaseDate} className="mr-2">→</button>
-
       </div>
-
     </div>
-    
+
     <br/>
     <br/>
+
+    <div className = "flex justify-center items-center w-[800px] h-[60px] p-4 bg-gray-10 border border-black ml-200px">
+      <p className = "mr-4"> Total Daily:</p>
+      <div className = "grid grid-cols-4 grid-rows-2 w-full max-w-md ">
+        <div className = "text-center">Cals</div>
+        <div className = "text-center">Protein</div>
+        <div className = "text-center">Fat</div>
+        <div className = "text-center">Carbs</div>
+        <div className = "text-center border border-black">0</div>
+        <div className = "text-center border border-black">0g</div>
+        <div className = "text-center border border-black">0g</div>
+        <div className = "text-center border border-black">0g</div>
+      </div>
+    </div>
+
+    <br/>
+    <br/>
+
+    <hr className="border-t border-black mx-200px" />
+
+
 
     {showModal && <DiaryModal />}
 
