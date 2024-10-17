d0adb82488450295c277eefb9e60f831b159fce7
FelixArbucias <91673165+nameYAY0@users.noreply.github.com>
2024-10-16 23:36:59 -0400

    added beginner code for diary modal

--- components/diary/DiaryModal.jsx
+++ components/diary/DiaryModal.jsx
@@ -1,5 +1,26 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
 const DiaryModal = () => {
-  return <div>DiaryModal</div>;
+
+
+    return (
+        <div style = {styles.container}>
+            <div
+                style = {{
+                    backgroundColor: 'gainsboro',
+                    padding: 10,
+                    borderRadius: 5,
+                    gap: 5,
+                    flexDirection: 'row',
+                }}
+            >
+                <div>
+                    <span style = {{fontWeight: 'bold', fontSize: 16}} >Pizza</span>
+                    <p style = {{color: 'dimgray' }}>350 Cal, Dominos</p>
+                </div>
+                <AntDesign name="pluscircleo" size={24} color="black" />
+            </div>
+        </div>
+    );
 };
 
 export default DiaryModal;
